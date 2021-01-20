aac614b1ee1da83e8961ab2f2c72baf95e7a832d
iroyo <iroyo.dev@protonmail.com>
2021-01-20 18:39:27 +0100

    moved ImageLoader into AsyncImage

--- wowpapers/Presentation/Common/AsyncImage.swift
+++ wowpapers/Presentation/Common/AsyncImage.swift
@@ -43,3 +43,33 @@ struct AsyncImage<Placeholder: View, Failure: View>: View {
         }
     }
 }
+
+class ImageLoader: ObservableObject {
+
+    enum LoadState {
+        case loading
+        case failure
+        case success(Data)
+    }
+
+    @Published var state = LoadState.loading
+
+    init(url: String) {
+        guard let parsedURL = URL(string: url) else {
+            fatalError("Invalid URL: \(url)")
+        }
+
+        let task = URLSession.shared.dataTask(with: parsedURL) { data, response, error in
+            if let data = data, data.count > 0 {
+                self.state = .success(data)
+            } else {
+                self.state = .failure
+            }
+
+            DispatchQueue.main.async {
+                self.objectWillChange.send()
+            }
+        }
+        task.resume()
+    }
+}
