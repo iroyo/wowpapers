f8e4f7c8339da026e23235a870da5d6ab5cfb0ff
iroyo <iroyo.dev@protonmail.com>
2021-01-20 21:19:56 +0100

    added custom modifier for outline CircularProgress

--- wowpapers/Presentation/Common/CircularProgress.swift
+++ wowpapers/Presentation/Common/CircularProgress.swift
@@ -6,20 +6,31 @@ import SwiftUI
 
 struct CircularProgress: View {
 
+    let size: CGFloat = 50
+    let color: Color = Color(red: 0.263, green: 0.667, blue: 0.545)
+
+    private var border: CGFloat {
+        (10 * size) / 100
+    }
+
+    private var radius: CGFloat {
+        (size / 2) - (border / 2)
+    }
+
     @State private var angle: Double = 0
     @State private var rotation: Double = -90
 
     var repeatingAnimation: Animation {
-        Animation.linear(duration: 3.5).repeatForever(autoreverses: false)
+        Animation.linear(duration: 6).repeatForever(autoreverses: false)
     }
 
     var body: some View {
-        CircularProgressShape(angle, rotation)
-                .stroke(Color.blue, lineWidth: 6)
+        CircularProgressShape(angle, rotation, radius)
+                .stroke(color, lineWidth: border)
                 .onAppear {
                     withAnimation(repeatingAnimation) {
                         self.angle = CircularProgressShape.MAX_ANGLE
-                        self.rotation = 180
+                        self.rotation = 540
                     }
                 }
     }
@@ -35,7 +46,7 @@ struct CircularProgressShape: Shape {
     private var angle: Double
     private var rotation: Double
 
-    init(_ angle: Double, _ rotation: Double, radius: CGFloat = 24) {
+    init(_ angle: Double, _ rotation: Double, _ radius: CGFloat) {
         circleRadius = radius
         self.angle = angle
         self.rotation = rotation
@@ -67,5 +78,31 @@ struct CircularProgressShape: Shape {
         }
         p.addArc(center: center, radius: circleRadius, startAngle: .degrees(start), endAngle: .degrees(end), clockwise: false)
         return p.rotation(.degrees(rotation)).path(in: rect)
+    }
+}
+
+struct Outline: ViewModifier {
+    var color: Color
+    var size: CGFloat
+    var border: CGFloat
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            Circle()
+                    .strokeBorder(color, lineWidth: border)
+                    .frame(width: size, height: size)
+            content
+        }
+    }
+}
+
+extension CircularProgress {
+
+    func withOutline() -> some View {
+        self.modifier(Outline(color: color.opacity(0.2), size: size, border: border))
+    }
+
+    func withOutline(color: Color) -> some View {
+        self.modifier(Outline(color: color, size: size, border: border))
     }
 }
