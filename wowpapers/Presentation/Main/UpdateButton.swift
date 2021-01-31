de706d3de6bf3e442118d9a3e972a3297ee0663e
iroyo <iroyo.dev@protonmail.com>
2021-01-31 09:31:36 +0100

    minor UpdateButton animation tweaks

--- wowpapers/Presentation/Main/UpdateButton.swift
+++ wowpapers/Presentation/Main/UpdateButton.swift
@@ -28,17 +28,20 @@ struct UpdateButtonStyle: ButtonStyle {
 
     let color: Color
 
+    private let size: CGFloat = 48
+    private var corners: CGFloat {
+        size / 2
+    }
+
     func makeBody(configuration: Configuration) -> some View {
-        let y: CGFloat = configuration.isPressed ? 0 : 4
-        let radius: CGFloat = configuration.isPressed ? 1.5 : 4
-        let shadowColor: Color = configuration.isPressed ? Color.black.opacity(0.25) : Color.black.opacity(0.45)
-        let animation: Animation = configuration.isPressed ? .default : Animation.interpolatingSpring(stiffness: 20, damping: 3).speed(2.5)
+        let y: CGFloat = configuration.isPressed ? 1 : 4
+        let animation: Animation = configuration.isPressed ? .default : Animation.interpolatingSpring(stiffness: 35, damping: 4.5).speed(2.5)
         return configuration.label
-                .frame(width: 48, height: 48)
+                .frame(width: size, height: size)
                 .background(color)
-                .cornerRadius(24)
-                .shadow(color: shadowColor, radius: radius, x: 0, y: y)
-                .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
+                .cornerRadius(corners)
+                .shadow(color: Color.black.opacity(0.45), radius: 4, x: 0, y: y)
+                .scaleEffect(configuration.isPressed ? 0.85 : 1.0)
                 .animation(animation)
 
     }
